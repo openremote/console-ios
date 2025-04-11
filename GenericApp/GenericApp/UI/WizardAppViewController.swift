4c196fa380f342a976dfa16bf52a9de7dafb21fd
Eric Bariaux <375613+ebariaux@users.noreply.github.com>
2025-04-11 10:19:06 +0200

    Use latest license header version, see https://github.com/openremote/openremote/pull/1441

--- GenericApp/GenericApp/UI/WizardAppViewController.swift
+++ GenericApp/GenericApp/UI/WizardAppViewController.swift
@@ -1,10 +1,21 @@
-//
-//  WizardAppViewController.swift
-//  GenericApp
-//
-//  Created by Eric Bariaux on 18/06/2022.
-//  Copyright © 2022 OpenRemote. All rights reserved.
-//
+/*
+ * Copyright 2025, OpenRemote Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 
 import UIKit
 import ORLib
